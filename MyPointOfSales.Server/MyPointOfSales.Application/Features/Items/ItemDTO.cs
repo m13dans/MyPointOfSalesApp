0383c7b58aae563023a24989c19c258bc1741d12
Danur Arya <danuraryapratama@gmail.com>
2024-10-30 14:14:18 +0700

    making property nullable

--- MyPointOfSales.Server/MyPointOfSales.Application/Features/Items/ItemDTO.cs
+++ MyPointOfSales.Server/MyPointOfSales.Application/Features/Items/ItemDTO.cs
@@ -6,7 +6,7 @@ public record ItemDTO
     public required decimal Harga { get; set; }
     public required int StokAwal { get; set; }
     public required string Kategori { get; set; }
-    public byte[] Gambar { get; set; }
-    public string AbsoluteUrlGambar { get; set; }
-    public string HostUrlGambar { get; set; }
+    public byte[]? Gambar { get; set; }
+    public string? AbsoluteUrlGambar { get; set; }
+    public string? HostUrlGambar { get; set; }
 }
