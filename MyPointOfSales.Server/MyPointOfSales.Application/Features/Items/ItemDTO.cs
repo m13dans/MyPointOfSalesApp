50621bea6ccd5663e5d7d9121da6ae4145ed60f4
Danur Arya <danuraryapratama@gmail.com>
2024-10-31 21:39:04 +0700

    remove unused item dto

--- MyPointOfSales.Server/MyPointOfSales.Application/Features/Items/ItemDTO.cs
+++ MyPointOfSales.Server/MyPointOfSales.Application/Features/Items/ItemDTO.cs
@@ -18,25 +18,11 @@ public record CreateItemRequest
     public required IFormFile Gambar { get; set; }
 }
 
-
-
 public record CreateItemResponse
 {
     public int Id { get; set; }
     public required string NamaBarang { get; set; }
     public string? UrlGambar { get; set; }
-}
-
-
-public record ItemDTO
-{
-    public required string NamaBarang { get; set; }
-    public required decimal Harga { get; set; }
-    public required int StokAwal { get; set; }
-    public required string Kategori { get; set; }
-    public byte[]? Gambar { get; set; }
-    public string? AbsoluteUrlGambar { get; set; }
-    public string? HostUrlGambar { get; set; }
 }
 
 public record UpdateItemRequest
